d4aa056edcdbfcf2ee237b62e05000eda2c1cd10
Chris Stalnley <chris.stalnley@janes.com>
2023-08-01 10:53:25 +0100

    messed up dates

--- pages/news1.js
+++ pages/news1.js
@@ -13,7 +13,7 @@ export default function News1() {
 
       <main className={styles.main}>
        <h1>Press release 1</h1>
-       <p className={styles.cardDate}>31 July 2023</p>
+       <p className={styles.cardDate}>1st Stangust 23</p>
        <p className={styles.cardCopy}>This is test content for press release one. Updated 30 July 2023</p>
       </main>
 
